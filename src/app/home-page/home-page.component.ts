227596c9b7bd20a7f653cc69c1abab4931b82b71
Partical <z25993021@gmail.com>
2020-02-23 13:41:21 +0800

    Modified introduction layout

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -23,6 +23,22 @@ export class HomePageComponent implements OnInit {
       content: "PersonalCard.Design/partical.weng"
     },
   ];
+  public introductionStaticData = [
+    "曾於金融科技業新創擔任 Angular 前端工程師，學會軟體協作流程及如何使用專案管理使團隊運作良好；也是在此確認自己的熱情是在於整合資源，製作產品或是幫助更適合的夥伴，而不僅僅是技術層面。",
+    "-space-",
+    "生物本科跨足前端之工程師，由豐富的線上課程、資源學習成材，甚至於2018年時，飛去美國矽谷參與一所非正規程式學校 - 42 長達一個月的 piscine 測驗並且通過測試。",
+    "-space-",
+    "熱衷學習不同領域知識與執行 Side Project 者，設計、行銷、商業模式、專案管理等等，藉由書籍與豐富的線上資源深入瞭解。",
+    "曾經主導小團隊製作了”戶外實境遊戲”。",
+    "也經營著不定時更新之技術部落格。",
+    "目前正進行著 PersonalCard.Design 的製作，Beta 版本已有 56 位出初期使用者。",
+    "-space-",
+    "平時也參與許多活動，像是新創盛會 Meet Taipei 以及許多大大小小的創新創業分享、展覽，曾經加入多個社群：Futureward, Mzone, 創客萊吧, 創客閣樓, Innoventure Club...。",
+    "-space-",
+    "愛好發掘國內外創新的產品、服務及商業模式，期待著創新將創造更美好的世界。固定關注 Product Hunt，以及其他社群與新聞平台。",
+    "-space-",
+    "相信廣泛學習與精深技能一樣重要，並且相信發展更多樣化的技能組合將是未來趨勢。"
+  ]
   public educationStaticData = [
     {
       title: "42 Silicon Valley",
@@ -125,6 +141,53 @@ export class HomePageComponent implements OnInit {
       title: "Maker 技術",
       contentList: [
         "曾經待過多個 Maker 空間&社群：Futureward, Mzone, 創客萊吧, 創客閣樓習得雷射切割雕刻，3D 列印，木工技術，以及許多開源電路板之應用。",
+      ]
+    },
+  ]
+  public moreProjectsStaticData = [
+    {
+      title: "42 piscine",
+      contentList: [
+        "參與一個月的 “Piscine” 測驗，在那一個月中每天都必須解決多個項目，並且每週擁有隨機組員的合作專案，以及每週的 Exam。",
+        "Github : https://github.com/Partical777/42piscine",
+        "Utilized : C programming, Unix, Git.",
+      ]
+    },
+    {
+      title: "Idea Manager",
+      contentList: [
+        "製作一個儲存及管理點子的網頁應用服務，特別針對“連結”的UX操作優化，讓使用者以更方便的方式管理搜集而來的網頁資訊。",
+        "Live: https://ideas-manager.stackblitz.io/",
+        "Utilized : Angular 7, Angular Material 2, Firebase Firestore, Google Sign In.",
+      ]
+    },
+    {
+      title: "Bal Laedi Project Musemaze : Real Escape Game",
+      contentList: [
+        "共同設計一個真實世界的實境遊戲，建立更沈浸的故事體驗與操作介面，讓每位玩家都可以透過手機，享受在另一個世界觀中。",
+        "Utilized : Front-end, Node.js, Bootstrap, jQuery, Google Sheet & App Script, Ajax, Catching, GPS, RWD Web Dev, Heroku."
+      ]
+    },
+    {
+      title: "News-Intergrated Chatbot",
+      contentList: [
+        "透過 Line Chatbot 及網頁爬蟲技術，整合多個優良科技新聞網站，每當有新聞發佈，將即時推送至手機。",
+        "Utilized : Web Crawing, Javascript, Node.js, Chatbot."
+      ]
+    },
+    {
+      title: "IoT Device Developement + Chatbot",
+      contentList: [
+        "開發透過 ChatBot 控制之物聯網裝置，透過 Line Chatbot 輸入指令，即可獲取感測器狀態，以及控制其行為。",
+        "Utilized : Javascript, Node.js, Arduino, MQTT, MCS, Linkit7697.",
+      ]
+    },
+    {
+      title: "Arduino RC Car with Android App",
+      contentList: [
+        "設計遙控車，透過 Arduino Uno, PWM 伺服馬達, HC-06 藍芽模組。",
+        "車身外殼透過 Illustrator 繪製及雷射切割木板製作，內部架構透過 3D 列印自行量測繪製。",
+        "Utilized : Arduino, Android, Laser Cutting, 3D Printing, Adobe Illustrator.",
       ]
     },
   ]
